a0564326075ad8de4875f0d23a599b6c4173510e
yastremskyn <yastremskyn@gmail.com>
2026-04-16 00:43:53 +0300

    cors

--- server.ts
+++ server.ts
@@ -30,7 +30,13 @@ async function startServer() {
   app.use(express.json());
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
     next();
   });
 
