df571a7a789aac1f513b9dd36e70b1707c258c7c
yastremskyn <yastremskyn@gmail.com>
2026-04-16 01:06:22 +0300

    Fix CORS OPTIONS preflight response

--- api/track.ts
+++ api/track.ts
@@ -13,13 +13,12 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
   );
 
   // Обробка preflight-запиту (OPTIONS)
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    return res.status(200).json({ body: "OK" });
   }
 
   if (req.method !== 'POST') {
